269702eb7c500e81ced42b04030c414d65be8a3e
talhabilal-dev <m.talha.bilal.dev@gmail.com>
2025-04-11 11:39:54 +0500

    feat: replace nodemailer with Mailgun for email sending functionality

--- src/lib/email.js
+++ src/lib/email.js
@@ -1,28 +1,26 @@
-import nodemailer from "nodemailer";
+import FormData from "form-data"; // form-data v4.0.1
+import Mailgun from "mailgun.js"; // mailgun.js v11.1.0
 
-export const sendEmail = async ({ to, text }) => {
+async function sendSimpleMessage() {
+  const mailgun = new Mailgun(FormData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.MAILGUN_API_KEY || "API_KEY",
+    // When you have an EU-domain, you must specify the endpoint:
+    // url: "https://api.eu.mailgun.net"
+  });
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
+    const data = await mg.messages.create("sandboxcde3da5bb42b474f9e1c41135b73ce80.mailgun.org", {
+      from: "Mailgun Sandbox <postmaster@sandboxcde3da5bb42b474f9e1c41135b73ce80.mailgun.org>",
+      to: ["Talha Bilal <m.talha.bilal.dev@gmail.com>"],
+      subject: "Hello Talha Bilal",
+      text: "Congratulations Talha Bilal, you just sent an email with Mailgun! You are truly awesome!",
     });
 
-    const info = await transporter.sendMail({
-      from: `"Whisperly" <${process.env.SMTP_USER}>`,
-      to,
-      subject: "Otp for Verification ",
-      text,
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    return { success: true, messageId: info.messageId };
+    console.log(data); // logs response data
   } catch (error) {
-    console.error("Error sending email:", error);
-    throw new Error("Error sending email");
+    console.log(error); //logs any error
   }
-};
+}
+
+export { sendSimpleMessage };
